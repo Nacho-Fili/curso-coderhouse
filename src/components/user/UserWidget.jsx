10a11d0dca47c3611ac82c313d6000fc8bcbcfa3
Ignacio Filipovskis <n.filipovskis@gmail.com>
2021-09-12 18:32:28 -0300

    style: estilo del widget de usuario en hover normalizado

--- src/components/user/UserWidget.jsx
+++ src/components/user/UserWidget.jsx
@@ -2,6 +2,7 @@ import React, {useContext, useState} from "react";
 import {AiOutlineUser} from "react-icons/ai";
 import {Link} from "react-router-dom";
 import UserContext from "../../context/UserContext";
+import styles from "./widget.module.scss";
 
 export default function UserWidget() {
   const {userLogged, logout} = useContext(UserContext);
@@ -9,7 +10,7 @@ export default function UserWidget() {
   const handleHover = () => setShowOptions(!showOptions);
 
   return (
-    <div onMouseEnter={handleHover} onMouseLeave={handleHover}>
+    <div className={styles.widgetContainer} onMouseEnter={handleHover} onMouseLeave={handleHover}>
       <Link to="/login">
         <AiOutlineUser size={25} style={{margin: "18px 0"}} />
       </Link>
@@ -18,7 +19,9 @@ export default function UserWidget() {
           <p className="clickable" onClick={logout}>
             Logout
           </p>
-          <Link to="/profile">Profile</Link>
+          <Link className={styles.profile} to="/profile">
+            Profile
+          </Link>
         </>
       )}
     </div>
